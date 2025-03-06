19cdf769252683771991618cd9c0905f2781d0c2
Vinny Neal <vinmanneal@gmail.com>
2025-03-06 10:52:42 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace WordSearch
+{
+    internal class Program
+    {
+        public const int sizeX = 20;
+        public const int sizeY = 20;
+
+        /// <summary>
+        /// The for loops in main makes the grid following the sixe parameters et by the two public ints
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    Console.Write("x ");
+                }
+
+                Console.Write('\n');
+            }
+        }
+
+    }
+}
