53dea11f0e606d0344d3382632e431013638dc26
Vinny Neal <vinmanneal@gmail.com>
2025-03-06 12:45:34 -0600

    Added text file and cross document text reader

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 ﻿namespace WordSearch
 {
-    internal class Program
+    internal class Program : Words
     {
-        public const int sizeX = 6;
-        public const int sizeY = 27;
-
+        public const int sizeX = 20;
+        public const int sizeY = 20;
+        
         /// <summary>
-        /// The for loops in main makes the grid following the sixe parameters et by the two public ints
+        /// The for loops in main makes the grid following the size parameters set by the two public int
         /// </summary>
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
             for (int i = 0; i < sizeX; i++)
-            {
-                for (int j = 0; j < sizeY; j++)
-                {
-                    Console.Write("x ");
-                }
+             {
+                 for (int j = 0; j < sizeY; j++)
+                 {
+                     Console.Write("X ");
+                 }
 
-                Console.Write('\n');
-            }
+                 Console.Write('\n');
+             }
+
+            ReadFromFile();
         }
 
     }
--- Words.cs
+++ Words.cs
@@ -8,5 +8,32 @@ namespace WordSearch
 {
     internal class Words
     {
+        /*/public Words() 
+        {
+            string[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string[] words = { "Cat", "Dog", "Owl", "Ferret", "Dove", "Sparrow", "Whale", };
+            string[] arrayIndex8 = new string[8];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arrayIndex8.Length; i++)
+            {
+                int randomIndex = rnd.Next(0, words.Length);
+                string potentialWord = words[randomIndex];
+                if (arrayIndex8.Contains(potentialWord))
+                {
+                    i--;
+                    continue;
+                }
+                arrayIndex8[i] = potentialWord;
+            }
+
+        }/*/
+
+        public static void ReadFromFile()
+        {
+            string linesFilePath = "Words.txt";
+            string[] linesRead = File.ReadAllLines(linesFilePath);
+            Console.WriteLine(linesRead);
+        }
     }
 }
